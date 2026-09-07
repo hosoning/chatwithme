f11ccf06377a2f60d8296e7ae60588e8946c8688
hosoning <hosoning@gmail.com>
2026-09-07 19:57:25 +0800

    Bump app cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tarot-wechat-v32';
+const CACHE_NAME = 'tarot-wechat-v33';
 const ASSETS = [
   './', './index.html',
   './js/tarot.js', './js/wordcards.js', './js/ai.js', './js/cloud.js', './js/app.js',
