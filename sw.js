9cad2aa1495ae7617a2a3df51146a19fe55e9a93
hosoning <hosoning@gmail.com>
2026-09-10 20:08:58 +0800

    Merge cloud chats without deleting replies

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ firebase.messaging().onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'tarot-wechat-v43';
+const CACHE_NAME = 'tarot-wechat-v44';
 const ASSETS = [
   './', './index.html',
   './js/tarot.js', './js/wordcards.js', './js/ai.js', './js/cloud.js', './js/app.js',
