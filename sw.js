1f2dde4f9306a967ca99e4a8ffe259b631097d04
hosoning <hosoning@gmail.com>
2026-09-10 20:27:36 +0800

    Add exclusive intimate word cards

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ firebase.messaging().onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'tarot-wechat-v44';
+const CACHE_NAME = 'tarot-wechat-v45';
 const ASSETS = [
   './', './index.html',
   './js/tarot.js', './js/wordcards.js', './js/ai.js', './js/cloud.js', './js/app.js',
