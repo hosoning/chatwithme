9e25bd73be9eaee8ec0523f5a372e46f900628cb
hosoning <hosoning@gmail.com>
2026-09-11 22:23:58 +0800

    Complete message tools and cloud sync (4/4)

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ firebase.messaging().onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'tarot-wechat-v46';
+const CACHE_NAME = 'tarot-wechat-v47';
 const ASSETS = [
   './', './index.html',
   './js/tarot.js', './js/wordcards.js', './js/ai.js', './js/cloud.js', './js/app.js',
