5a7f5196cb496370e3f0c39b2747e78a4e60481e
hosoning <hosoning@gmail.com>
2026-09-16 19:43:00 +0800

    Bump PWA cache to v52

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ firebase.messaging().onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'tarot-wechat-v51';
+const CACHE_NAME = 'tarot-wechat-v52';
 const ASSETS = [
   './', './index.html',
   './js/tarot.js', './js/wordcards.js', './js/dictionary.js', './js/ai.js', './js/cloud.js', './js/app.js',
